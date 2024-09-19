6927c35a72949fb18d26365146ea13f48abe5cf9
Benjamin McMaster <benjamin.mcmaster@rdm.ox.ac.uk>
2024-09-19 13:11:04 +0100

    Add app to extract specific chains from a pdb into a new file

---
+++ src/tcr_antigen_prediction/apps/extract_chains_from_structure.py
@@ -0,0 +1,36 @@
+'''Extract chains from a PDB structue into a new PDB file.'''
+import argparse
+import logging
+
+from Bio.PDB import PDBParser, PDBIO
+
+from tcr_antigen_prediction.apps._log import add_logging_arguments, setup_logger
+from tcr_antigen_prediction.structure import extract_chains
+
+logger = logging.getLogger()
+
+parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+
+parser.add_argument('structure', help='path to pdb file')
+parser.add_argument('--output', '-o', required=True, help='path to output pdb file')
+parser.add_argument('--chains', nargs='+', required=True, help='chains to extract')
+
+add_logging_arguments(parser)
+
+
+def main():
+    args = parser.parse_args()
+    setup_logger(logger, args.log_level)
+
+    pdb_parser = PDBParser(QUIET=True)
+    structure = pdb_parser.get_structure('', args.structure)
+
+    output_structure = extract_chains(structure, args.chains)
+
+    io = PDBIO()
+    io.set_structure(output_structure)
+    io.save(args.output)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/apps/extract_chains_from_structure/test_extract_chains_from_structure.t
@@ -0,0 +1,7 @@
+Test 1oa7 chains D E C A B
+  $ python -m tcr_antigen_prediction.apps.extract_chains_from_structure \
+  > --chains D E C A B \
+  > -o 1ao7_DECAB.pdb \
+  > $TESTDIR/data/1ao7.pdb
+
+  $ diff 1ao7_DECAB.pdb $TESTDIR/reference/1ao7_DECAB.pdb
