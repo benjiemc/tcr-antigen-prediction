43751de49c11145cfe20a0d0c1a8f254ab4087fc
Benjamin McMaster <benjamin.mcmaster@rdm.ox.ac.uk>
2024-11-22 16:20:51 +0000

    Update sklearn keyword

--- src/tcr_antigen_prediction/apps/filter_similar_structures.py
+++ src/tcr_antigen_prediction/apps/filter_similar_structures.py
@@ -49,7 +49,7 @@ def remove_similar_structures(df: pd.DataFrame,
         chain_maps = group.filter(regex=r'\w+chain\w*').to_dict('records')
 
         distance_matrix = compute_structural_distances(group_structures, chain_maps, mhc_type)
-        clusters = AgglomerativeClustering(affinity='precomputed',
+        clusters = AgglomerativeClustering(metric='precomputed',
                                            distance_threshold=threshold,
                                            linkage='single',
                                            n_clusters=None).fit(distance_matrix).labels_
